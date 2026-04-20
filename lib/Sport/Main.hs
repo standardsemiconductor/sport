7faaf22e8e77a074ce80c4b3c556a2c974cce87e
dopamane <dwc1295@gmail.com>
2026-04-20 06:38:04 -0700

    update main

--- lib/Sport/Main.hs
+++ lib/Sport/Main.hs
@@ -2,21 +2,23 @@ module Sport.Main (sportMain) where
 
 import Control.Concurrent.Async
 import Control.Monad
+import qualified Data.ByteString.Lazy.Char8 as BS
 import Sport.Serial
+import Sport.Sport
 import System.IO
 import System.Posix
 
 sportMain :: IO ()
-sportMain = do
-  putStrLn "...UNIX.SERIAL.PORT..."
-  withSerial defSerialConfig{speed = B19200} $ \serial -> do
-    hSetBuffering serial NoBuffering
+sportMain =
+  withSport $ \s -> do
+    putStrLn "...UNIX.SERIAL.PORT..."
     hSetBuffering stdin  NoBuffering
     hSetBuffering stdout NoBuffering
-    concurrently_ (reading serial) (writing serial)
+    openSport s defSportConfig{serialConfig = defSerialConfig{speed = B19200}}
+    concurrently_ (reading s) (writing s)
 
-reading :: Handle -> IO a
-reading serial = forever $ hGetChar serial >>= putChar
+reading :: Sport -> IO a
+reading s = forever $ putChar . BS.head =<< readSport s 1
 
-writing :: Handle -> IO a
-writing serial = forever $ getChar >>= hPutChar serial
+writing :: Sport -> IO a
+writing s = forever $ writeSport s . BS.singleton =<< getChar
