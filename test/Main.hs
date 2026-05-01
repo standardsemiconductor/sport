4b4fcb904f6adcb477ca1469fc0aea0f03f5b2c9
dopamane <dwc1295@gmail.com>
2026-04-30 23:19:46 -0700

    sport testing

--- test/Main.hs
+++ test/Main.hs
@@ -1,16 +1,26 @@
 module Main (main) where
 
 import Control.Concurrent.Async
+import Control.Concurrent.STM
 import qualified Data.ByteString.Lazy as BS
 import Sport
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "SPORT TEST"
   s <- newSportIO
   race_ (runSport s) $ do
+    putStr "OPENING..."
     openSport s defSportCfg{speed=B19200}
+    putStrLn "DONE"
     let bs = BS.replicate 256 0xDC
-    writeSport s bs
-    bs' <- readSport s 256
-    putStrLn $ if bs == bs' then "PASS" else "FAIL"
+    putStr "XFERING..."
+    bs' <- fst <$> concurrently (readSport s 10) (writeSport s bs)
+    putStrLn "DONE"
+    putStr "COMPARING..."
+    putStrLn $ if BS.take 10 bs == bs' then "PASS" else "FAIL"
+    putStr "CLOSING..."
     closeSport s
+    putStrLn "DONE"
